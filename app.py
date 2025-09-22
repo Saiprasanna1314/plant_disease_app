ae5f4b4d83d3597ac0e62d1990349e4adf74699e
Saiprasanna1314 <saiprasannachekuri@gmail.com>
2025-09-22 22:35:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,6 @@ def predict():
     return render_template('index.html', prediction=label,  img_path=save_path)
 
 if __name__ == "__main__":
-    app.run(debug=True)                   
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or 5000 as fallback
+    app.run(host="0.0.0.0", port=port, debug=True)
+                 
